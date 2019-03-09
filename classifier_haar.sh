46df833e13c304dadb5fe404c20c1182c095ebd6
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-09 23:07:43 +0530

    Added both classifiers.
    Former-commit-id: 9b8be8f01f71a9ff4f6738554580493c43d149cd

--- classifier_haar.sh
+++ classifier_haar.sh
@@ -11,5 +11,5 @@ opencv_traincascade -data classifier -vec samples.vec -bg negatives.txt\
    -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 1000\
    -numNeg 600 -w 80 -h 40 -mode ALL \
    -precalcValBufSize 1024\
-   -precalcIdxBufSize 1024 # -featureType LBP
+   -precalcIdxBufSize 1024
 
